6621fdc47af9928a9b89c38f158af262f400431f
dhanrajdc7 <dc6707914@gmail.com>
2021-10-31 23:18:14 +0530

    Initial Commit

---
+++ Unmutify/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Unmutify
+//
+//  Created by Dhanraj Chavan on 31/10/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Unmutify/UnmutifyApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnmutifyApp.swift
+//  Unmutify
+//
+//  Created by Dhanraj Chavan on 31/10/21.
+//
+
+import SwiftUI
+
+@main
+struct UnmutifyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
